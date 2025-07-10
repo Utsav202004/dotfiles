d5484b946352c29aa95e7e5d9f01a32ac0b4102e
Utsav Goyal <goyalutsav2004@gmail.com>
2025-07-10 15:12:15 +0530

    Fixed errors in install.sh file

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/bash/bin
+#!/bash/bash
 
 # Creating symbolic links
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.vimrc ~/.vimrc.
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
